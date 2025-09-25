b6582dfd820fd33e14a4fb41a7d6f0b4d5d4aad1
Prashan5903s <chaubeyprashant498@gmail.com>
2025-09-25 10:43:05 +0530

    Changes of society dashboard

---
+++ controller/Company/DashboardController.js
@@ -0,0 +1,151 @@
+const Bill = require('../../model/Bill')
+const User = require('../../model/User')
+const Tower = require('../../model/Tower')
+const Visitor = require('../../model/Visitor')
+const UserBill = require('../../model/UserBill')
+const Maintenance = require("../../model/Maintenance")
+const Complain = require('../../model/Complain')
+const Apartment = require('../../model/Apartment');
+const { errorResponse, successResponse } = require('../../util/response');
+
+async function getComplainsByStatus(userIds, status) {
+    return await Complain.aggregate([
+        {
+            $match: { created_by: { $in: userIds } }
+        },
+        {
+            $lookup: {
+                from: "complain_users",
+                let: { complainId: "$_id" },
+                pipeline: [
+                    {
+                        $match: { $expr: { $eq: ["$complain_id", "$$complainId"] } }
+                    },
+                    { $sort: { created_at: -1 } }, // latest first
+                    { $limit: 1 } // केवल latest record
+                ],
+                as: "latest_complain_user"
+            }
+        },
+        {
+            $unwind: { path: "$latest_complain_user", preserveNullAndEmptyArrays: true }
+        },
+        {
+            $match: { "latest_complain_user.complaint_status": status }
+        }
+    ]);
+}
+
+exports.getDashboardDataAPI = async (req, res, next) => {
+    try {
+
+        const userId = req.userId;
+
+        const tower = await Tower.find({ created_by: userId })
+
+        const apartment = await Apartment.find({ created_by: userId })
+
+        const unsoldApartment = await Apartment.find({ created_by: userId, status: false })
+
+        const owner = await User.find({ created_by: userId })
+
+        const user = await User.find({ created_by: userId }).select('_id')
+        const userIds = user.map(o => o._id.toString())
+
+        const pendingComplain = await getComplainsByStatus(userIds, "1");
+        const inProgressComplain = await getComplainsByStatus(userIds, "4");
+        const resolvedComplain = await getComplainsByStatus(userIds, "3");
+
+        const paidCommanAreaBill = await Bill.find({ status: true, bill_data_type: "common-area-bill", created_by: userId }).populate('apartment_id').populate('bill_type').populate({
+            path: "payments",
+            model: "Payment"
+        });
+
+        const unpaidCommanAreaBill = await Bill.find({
+            status: false,
+            bill_data_type: "common-area-bill",
+            created_by: userId
+        })
+            .populate('apartment_id')
+            .populate('bill_type')
+            .populate({
+                path: "payments",
+                model: "Payment"
+            });
+
+
+        const utilityBill = await Bill.find({ user_id: userId, bill_data_type: "utilityBills" }).populate('apartment_id').populate('bill_type').populate({
+            path: "payments",
+            model: "Payment"
+        });
+
+        const paidUtilityBill = await Bill.find({ user_id: userId, status: true, bill_data_type: "utilityBills" }).populate('apartment_id').populate('bill_type').populate({
+            path: "payments",
+            model: "Payment"
+        });
+        const unpaidUtilityBill = await Bill.find({ user_id: userId, status: false, bill_data_type: "utilityBills" }).populate('apartment_id').populate('bill_type').populate({
+            path: "payments",
+            model: "Payment"
+        });
+
+        const startOfToday = new Date();
+        startOfToday.setHours(0, 0, 0, 0);
+
+        const endOfToday = new Date();
+        endOfToday.setHours(23, 59, 59, 999);
+
+        const visitor = await Visitor.find({
+            created_by: { $in: userIds },
+            created_at: {
+                $gte: startOfToday,
+                $lte: endOfToday
+            }
+        })
+            .populate('user_id')
+            .populate({
+                path: 'apartment_id',
+                populate: [
+                    { path: 'tower_id' },
+                    { path: 'floor_id' }
+                ]
+            });
+
+        const userBill = await UserBill.find({ user_id: { $in: userIds } })
+            .populate('bill_id')
+            .populate('apartment_id')
+            .populate('user_id')
+
+        const maintenance = await Maintenance.findOne({ cost_type: "1" })
+
+        const fixedCost = maintenance.fixed_data;
+
+        if (!fixedCost || !maintenance || !userBill || !visitor || !tower || !apartment || !unsoldApartment || !owner || !pendingComplain || !resolvedComplain || !inProgressComplain || !paidCommanAreaBill || !unpaidCommanAreaBill || !unpaidUtilityBill || !paidUtilityBill || !utilityBill) {
+            return errorResponse(res, "Dashboard data does not exist", {}, 404)
+        }
+
+        const data = {
+            fixedCost,
+            maintenance,
+            userBill,
+            visitor,
+            tower,
+            apartment,
+            unsoldApartment,
+            owner,
+            pendingComplain,
+            resolvedComplain,
+            inProgressComplain,
+            paidCommanAreaBill,
+            unpaidCommanAreaBill,
+            paidUtilityBill,
+            unpaidUtilityBill,
+            utilityBill
+        }
+
+        return successResponse(res, "Dashboard data does not exist", data)
+
+
+    } catch (error) {
+        next(error)
+    }
+}
