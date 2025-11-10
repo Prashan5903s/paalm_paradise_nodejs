9470d67eccce2957446a3a60f91c3f186aff86fb
Prashan5903s <chaubeyprashant498@gmail.com>
2025-11-10 18:54:34 +0530

    Changes of visitor create

--- controller/User/VisitorController.js
+++ controller/User/VisitorController.js
@@ -178,15 +178,22 @@ exports.getVisitorFilterController = async (req, res, next) => {
         }
 
         const visitors = await Visitor.find(filter)
-            .populate('user_id')
-            .populate('category')
+            .populate[{
+                path: "user_id",
+                select: "_id first_name last_name"
+            }, {
+                path: "category",
+                select: "_id name"
+            }]
             .populate({
                 path: 'apartment_id',
                 populate: [{
-                        path: 'tower_id'
+                        path: 'tower_id',
+                        select: "_id name" // adjust as needed
                     },
                     {
-                        path: 'floor_id'
+                        path: 'floor_id',
+                        select: "_id floor_name" // adjust as needed
                     }
                 ]
             });
